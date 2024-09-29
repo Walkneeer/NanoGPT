8c51a3725f4d204ed16ceeeaaa952230c7a05662
Nicolas Lamour <118196793+Walkneeer@users.noreply.github.com>
2024-09-29 11:21:38 +0200

    v2

--- V2.py
+++ V2.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
+batch_size = 16 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
